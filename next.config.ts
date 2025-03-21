e4546dedab6a536386b383bffc8f31c96746c04e
bonesed <40335001+bonesed@users.noreply.github.com>
2025-03-22 07:37:59 +0900

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,10 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  output: "export", // ✅ これを追加する！
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: "export",
 };
 
 export default nextConfig;
+
 
 
 
