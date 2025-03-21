44972f9c29b5a7a8fa728b29afbd9490f399775e
bonesed <40335001+bonesed@users.noreply.github.com>
2025-03-22 00:10:40 +0900

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "export", // Next.jsの静的エクスポートを有効化
+  images: {
+    unoptimized: true, // 画像最適化を無効化（エラー回避）
+  },
 };
 
 export default nextConfig;
+
