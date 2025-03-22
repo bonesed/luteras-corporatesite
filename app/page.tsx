dffae733301975ccba4ef0d9513c87383dd1ad18
bonesed <40335001+bonesed@users.noreply.github.com>
2025-03-22 17:50:14 +0900

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,4 @@
 
-export default function Home() {
-  return (
-    <main>
-      <h1>Welcome to My Next.js App</h1>
-      <p>This is the homepage.</p>
-    </main>
-  );
-}
 
 import Image from "next/image"
 import Link from "next/link"
